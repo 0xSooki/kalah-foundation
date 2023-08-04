82bd862dbf8e2e886530f2191c2c9993802a78f0
Sooki <0xSooki@gmail.com>
2023-08-04 20:31:17 +0200

    refactor

--- packages/nextjs/src/pages/_app.tsx
+++ packages/nextjs/src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import 'tailwindcss/tailwind.css'
 import { ThemeProvider } from 'next-themes'
 import Provider from '@/components/shared/Provider'
+import Header from '@/components/shared/Header'
 
 const App = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider attribute="class">
 			<Provider>
-				<Component {...pageProps} />
+				<div className="flex flex-col min-h-screen">
+					<Header />
+					<main className="">
+						<Component {...pageProps} />
+					</main>
+				</div>
 			</Provider>
 		</ThemeProvider>
 	)
--- packages/nextjs/src/pages/index.tsx
+++ packages/nextjs/src/pages/index.tsx
@@ -1,12 +1,10 @@
-import Header from '@/components/shared/Header'
 import { FC } from 'react'
 import Lock from '@/components/Lock'
 
 const Home: FC = () => {
 	return (
 		<>
-			<Header />
-			<div className="relative flex items-top justify-center min-h-screen bg-light dark:bg-dark sm:items-center py-4 sm:pt-0">
+			<div className=" bg-light dark:bg-dark w-full sm:items-center py-4 sm:pt-0">
 				<div className="flex flex-col items-center justify-center min-h-screen py-2">
 					<Lock />
 				</div>
