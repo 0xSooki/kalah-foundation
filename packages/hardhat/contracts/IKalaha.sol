444da9dc51a8c903376111678b3a0dfc71655ddc
Sooki <0xSooki@gmail.com>
2023-08-04 20:41:00 +0200

    add modifiers & events

--- packages/hardhat/contracts/IKalaha.sol
+++ packages/hardhat/contracts/IKalaha.sol
@@ -2,9 +2,11 @@
 pragma solidity ^0.8.0;
 
 abstract contract IKalaha {
-
     event NewGame(uint256 _game, address _by);
     event Join(uint256 _game, address _by);
+    event Move(uint256 _game, address _by, uint8 x);
+    event Win(uint256 _game, address _winner, address _by);
+    event Start(uint256 _game, address _by);
 
     struct Game {
         address[2] players;
@@ -13,17 +15,24 @@ abstract contract IKalaha {
         address winner;
     }
     uint256 public gameID;
-    mapping (uint256 => Game) games;
+    mapping(uint256 => Game) games;
+
     function state(
         uint256 _game
-        )
-        external view virtual returns (
+    )
+        external
+        view
+        virtual
+        returns (
             address[2] memory players,
             uint8[14] memory board,
             uint8 nonce,
             address winner
         );
+
     function newGame() external virtual;
+
     function join(uint256 _game) external virtual;
+
     function move(uint256 _game, uint8 x) external virtual;
 }
--- packages/hardhat/contracts/Kalaha.sol
+++ packages/hardhat/contracts/Kalaha.sol
@@ -1,15 +1,25 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "./IKalaha.sol";
 
 contract Kalaha is IKalaha {
+    modifier myTurn(uint256 _game) {
+        require(
+            games[_game].players[games[_game].nonce % 2] == msg.sender,
+            "Not your turn"
+        );
+        _;
+    }
 
     function state(
         uint256 _game
     )
-        external view virtual override returns (
+        external
+        view
+        virtual
+        override
+        returns (
             address[2] memory players,
             uint8[14] memory board,
             uint8 nonce,
@@ -23,49 +33,46 @@ contract Kalaha is IKalaha {
             games[_game].winner
         );
     }
-    function newGame() external virtual override  {
+
+    function newGame() external virtual override {
         gameID++;
         games[gameID].players[0] = msg.sender;
-        games[gameID].board = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
+        games[gameID].board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
         games[gameID].nonce = 0;
     }
-    function join(uint256 _game) external virtual override  {
+
+    function join(uint256 _game) external virtual override {
         //modifier
         games[_game].players[1] = msg.sender;
-        emit Join(_game,msg.sender);
+        emit Join(_game, msg.sender);
     }
-    function move(
-        uint256 _game, 
-        uint8 x
-        ) external virtual override {
-            //modifier
-            uint8[14] memory board = games[_game].board;
-            if (games[_game].nonce % 2 == 0) {
-                uint8 t = board[6-x];
-                board[6-x] = 0;
-                for (uint i = 1; i < t+1; i++) {
-                    board[(6-x+i)%14]++;
-                }
-                if ((6-x+t)%14 == 5) {
-                    games[_game].nonce += 2;
-                }
-                else {
-                    games[_game].nonce++;
-                }
+
+    function move(uint256 _game, uint8 x) external virtual override {
+        //modifier
+        uint8[14] memory board = games[_game].board;
+        if (games[_game].nonce % 2 == 0) {
+            uint8 t = board[6 - x];
+            board[6 - x] = 0;
+            for (uint i = 1; i < t + 1; i++) {
+                board[(6 - x + i) % 14]++;
             }
-            else {
-                uint8 t = board[14-x];
-                board[14-x] = 0;
-                for (uint i = 1; i < t+1; i++) {
-                    board[(i-x)%14]++;
-                }
-                if ((t-x)%14 == 13) {
-                    games[_game].nonce += 2;
-                }
-                else {
-                    games[_game].nonce++;
-                }
+            if ((6 - x + t) % 14 == 5) {
+                games[_game].nonce += 2;
+            } else {
+                games[_game].nonce++;
             }
-            games[_game].board = board;
+        } else {
+            uint8 t = board[14 - x];
+            board[14 - x] = 0;
+            for (uint i = 1; i < t + 1; i++) {
+                board[(i - x) % 14]++;
+            }
+            if ((t - x) % 14 == 13) {
+                games[_game].nonce += 2;
+            } else {
+                games[_game].nonce++;
+            }
         }
+        games[_game].board = board;
+    }
 }
