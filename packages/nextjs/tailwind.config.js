a2304e0d4b7294341ecae71c2658554b7fc76156
Sooki <0xSooki@gmail.com>
2023-08-04 20:26:00 +0200

    create default theme & colours

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -1,9 +1,34 @@
-/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	plugins: [require('daisyui')],
 	darkMode: 'class',
+	daisyui: {
+		styled: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: '',
+		themes: [
+			{
+				default: {
+					primary: '#64230D',
+					secondary: '#FCF2C1',
+				},
+			},
+		],
+	},
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: '#FF822C',
+				darkest: '#240C04',
+				dark: '#64230D',
+				mid: '#FDE047',
+				light: '#FCF2C1',
+				lightest: '#FFFFFF',
+			},
+		},
 	},
 }
