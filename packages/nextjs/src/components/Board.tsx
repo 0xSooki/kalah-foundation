17f25ea1860abd90e4c04af4296f9d005d994d2c
Sooki <0xSooki@gmail.com>
2023-08-06 19:24:22 +0200

    add siwe

--- packages/nextjs/src/components/Board.tsx
+++ packages/nextjs/src/components/Board.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Board = ({ players, board, nonce, winner }) => {
+	
 	return (
 		<div className="flex items-center space-y-4 dark:bg-light bg-dark lg:p-8 p-4 rounded-lg">
 			<div className="flex flex-col space-y-4">
--- packages/nextjs/src/pages/_app.tsx
+++ packages/nextjs/src/pages/_app.tsx
@@ -1,19 +1,59 @@
 import 'tailwindcss/tailwind.css'
 import { ThemeProvider } from 'next-themes'
-import Provider from '@/components/shared/Provider'
-import Header from '@/components/header/Header'
+import { RainbowKitSiweNextAuthProvider } from '@rainbow-me/rainbowkit-siwe-next-auth'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { SessionProvider } from 'next-auth/react'
+import type { Session } from 'next-auth'
+import { AppProps } from 'next/app'
+import { WagmiConfig } from 'wagmi'
+import '@rainbow-me/rainbowkit/styles.css'
+import { darkTheme, getDefaultWallets, lightTheme } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { sepolia, hardhat, localhost } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import { useTheme } from 'next-themes'
+import { ALCHEMY_ID, WALLET_CONNECT_ID } from '@/lib/consts'
 
-const App = ({ Component, pageProps }) => {
+const { chains, publicClient } = configureChains(
+	[sepolia, hardhat, localhost],
+	[alchemyProvider({ apiKey: ALCHEMY_ID }), publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+	appName: 'kalah',
+	projectId: WALLET_CONNECT_ID,
+	chains,
+})
+const wagmiConfig = createConfig({
+	autoConnect: true,
+	connectors,
+	publicClient,
+})
+
+const App = ({
+	Component,
+	pageProps,
+}: AppProps<{
+	session: Session
+}>) => {
+	const { theme } = useTheme()
+
 	return (
 		<ThemeProvider attribute="class">
-			<Provider>
-				<div className="flex flex-col min-h-screen">
-					<Header />
-					<main className="">
-						<Component {...pageProps} />
-					</main>
-				</div>
-			</Provider>
+			<WagmiConfig config={wagmiConfig}>
+				<SessionProvider refetchInterval={0} session={pageProps.session}>
+					<RainbowKitSiweNextAuthProvider>
+						<RainbowKitProvider
+							theme={theme == 'light' ? lightTheme() : darkTheme()}
+							modalSize="compact"
+							chains={chains}
+						>
+							<Component {...pageProps} />
+						</RainbowKitProvider>
+					</RainbowKitSiweNextAuthProvider>
+				</SessionProvider>
+			</WagmiConfig>
 		</ThemeProvider>
 	)
 }
