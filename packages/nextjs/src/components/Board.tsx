cd02ccda5a559e5f1bf07481fdd0fa244ac8363e
Sooki <0xSooki@gmail.com>
2023-08-07 22:28:51 +0200

    format

--- packages/nextjs/src/components/Board.tsx
+++ packages/nextjs/src/components/Board.tsx
@@ -6,12 +6,12 @@ export const Board = ({ gameID, players, board, nonce, winner }) => {
 		<div className="flex items-center space-y-4 dark:bg-light bg-dark lg:p-8 p-4 rounded-lg">
 			<div className="flex flex-col space-y-4">
 				<div className="flex font-rubik text-xl dark:text-light text-dark space-x-4">
-					<UnifiedPocket value={board[6]} />
+					<UnifiedPocket value={board[13]} />
 					<div className="flex flex-col space-y-4">
 						<div className="flex space-x-4">{renderPockets(7, board, gameID).reverse()}</div>
 						<div className="flex space-x-4">{renderPockets(0, board, gameID)}</div>
 					</div>
-					<UnifiedPocket value={board[13]} />
+					<UnifiedPocket value={board[6]} />
 				</div>
 			</div>
 		</div>
@@ -62,7 +62,7 @@ export const HandleMove = ({ gameID, value, id }) => {
 			}}
 			className="lg:w-24 lg:h-24 h-12 w-12 dark:bg-dark bg-light rounded-2xl flex justify-center items-center"
 		>
-			{value.value}
+			{value}
 		</button>
 	)
 }
