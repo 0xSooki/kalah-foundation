0cad73efcd68614b2c0c89e336252ad26d256a97
Sooki <0xSooki@gmail.com>
2023-08-09 08:36:01 +0200

    fix logo in header

--- packages/nextjs/src/components/header/Header.tsx
+++ packages/nextjs/src/components/header/Header.tsx
@@ -5,6 +5,7 @@ import { MENU_LIST } from '@/lib/consts'
 import Logo from '../shared/Logo'
 import Image from 'next/image'
 import logo from 'public/images/logo.png'
+import KalahFoundationLogo from 'public/images/kalah-foundation-logo.svg'
 
 const Header = () => {
 	return (
@@ -12,8 +13,8 @@ const Header = () => {
 			<div className="navbar absolute top-0 z-10 min-h-0 dark:bg-dark bg-light">
 				<div className="flex-1">
 					<Image src={logo} alt="logo" width={50} height={50} />
-					<Link className="normal-case font-bold ml-2 font-signika mr-8 text-primary text-3xl" href="/">
-						<Logo />
+					<Link className="normal-case font-bold ml-2 font-signika mr-8 dark:text-light text-dark text-3xl" href="/">
+						<KalahFoundationLogo />
 					</Link>
 					<ul className="flex items-center flex-row gap-6">
 						{MENU_LIST.map(item => {
