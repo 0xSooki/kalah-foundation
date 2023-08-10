cac41d9e63f16051305d8fca385cd2330f65a89e
Sooki <0xSooki@gmail.com>
2023-08-10 22:19:23 +0200

    make navbar responsive

--- packages/nextjs/src/components/header/Header.tsx
+++ packages/nextjs/src/components/header/Header.tsx
@@ -1,22 +1,28 @@
 import Link from 'next/link'
 import { ConnectButton } from './ConnectButton'
-import ThemeSwitcher from './ThemeSwitcher'
 import { MENU_LIST } from '@/lib/consts'
-import Logo from '../shared/Logo'
 import Image from 'next/image'
 import logo from 'public/images/logo.png'
 import KalahFoundationLogo from 'public/images/kalah-foundation-logo.svg'
+import ThemeSwitcher from './ThemeSwitcher'
 
 const Header = () => {
 	return (
-		<>
-			<div className="navbar absolute top-0 z-10 min-h-0 dark:bg-dark bg-light">
-				<div className="flex-1">
-					<Image src={logo} alt="logo" width={50} height={50} />
-					<Link className="normal-case font-bold ml-2 font-signika mr-8 dark:text-light text-dark text-3xl" href="/">
-						<KalahFoundationLogo />
-					</Link>
-					<ul className="flex items-center flex-row gap-6">
+		<div className="navbar top-0 z-10 absolute min-h-0 dark:bg-dark bg-light">
+			<div className="navbar-start">
+				<div className="dropdown">
+					<label tabIndex={0} className="btn btn-ghost lg:hidden">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5 dark:text-light text-dark"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+						</svg>
+					</label>
+					<ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
 						{MENU_LIST.map(item => {
 							return (
 								<li key={item.name}>
@@ -31,13 +37,37 @@ const Header = () => {
 						})}
 					</ul>
 				</div>
-				<ul className="flex items-center justify-center flex-row gap-6">
-					<li className="flex justify-center items-center gap-2 flex-row">
-						<ThemeSwitcher /> <ConnectButton />
-					</li>
-				</ul>
+
+				<a className="flex font-bold normal-case ml-2 dark:text-light text-dark text-3xl">
+					<Image className="mr-4" src={logo} alt="logo" width={50} height={50} />
+
+					<div className="mr-12">
+						<KalahFoundationLogo />
+					</div>
+				</a>
+
+				<div className="navbar top-0 z-10 min-h-0 hidden lg:flex">
+					<ul className=" px-1">
+						{MENU_LIST.map(item => {
+							return (
+								<li key={item.name}>
+									<Link
+										className="btn btn-sm btn-ghost normal-case font-bold text-lg text-dark dark:text-light"
+										href={item.path}
+									>
+										{item.name}
+									</Link>
+								</li>
+							)
+						})}
+					</ul>
+				</div>
 			</div>
-		</>
+			<div className="navbar-end">
+				<ThemeSwitcher className="mr-2" />
+				<ConnectButton />
+			</div>
+		</div>
 	)
 }
 
